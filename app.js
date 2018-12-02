4f92b0600c887d127a9067b4fc98feca6ba2b8b8
Tyler Tompa <tylertompa@email.com>
2018-12-02 12:54:29 -0500

    produce table from data

--- app.js
+++ app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+data.forEach(function(aliens) {
+  console.log(aliens)
+});
+
+var tbody = d3.select("tbody");
+
+data.forEach(function(alien_reporter) {
+  var row = tbody.append("tr");
+  Object.entries(alien_reporter).forEach(function([key, value])
+  {
+      var cell = row.append("td");
+      cell.text(value)
+  }
+  )
+});
